db7feb19478e807e3b5792392a82943f4a90b448
David Twigger <d.twigger@bee-buzziness.com>
2015-11-13 13:31:08 +0100

    replace tabs with spaces

--- src/thermometer.js
+++ src/thermometer.js
@@ -1,53 +1,53 @@
 (function() {
-	var fs = require('fs-extra-promise')
-	var path = require("path")
-	var _ = require('lodash');
+    var fs = require('fs-extra-promise')
+    var path = require("path")
+    var _ = require('lodash');
 
-	function Thermometer(settings) {
-		defaultSettings = {
-			device: "w1_slave",
-			devicePath: "/sys/bus/w1/devices/"
-		}
-		this.settings = _.assign(defaultSettings, settings)
-	}
+    function Thermometer(settings) {
+        defaultSettings = {
+            device: "w1_slave",
+            devicePath: "/sys/bus/w1/devices/"
+        }
+        this.settings = _.assign(defaultSettings, settings)
+    }
 
-	Thermometer.prototype.getThermometerData = function(devicePath) {
-		var settings = this.settings
+    Thermometer.prototype.getThermometerData = function(devicePath) {
+        var settings = this.settings
 
-		var file = path.join(devicePath || settings.devicePath, settings.device)
-		file = path.normalize(file)
+        var file = path.join(devicePath || settings.devicePath, settings.device)
+        file = path.normalize(file)
 
-		if(fs.lstatSync(file).isFile()) {
-			return fs.readFileAsync(file, 'utf8')
-		} else {
-			return Promise.reject(new Error(file + ' doesn\'t exist!'))
-		}
-	}
+        if(fs.lstatSync(file).isFile()) {
+            return fs.readFileAsync(file, 'utf8')
+        } else {
+            return Promise.reject(new Error(file + ' doesn\'t exist!'))
+        }
+    }
 
-	Thermometer.prototype.checkCrc = function(data) {		
-		if(/.*([a-fA-F0-9]{2})\s:\scrc=([0-9]+)\sYES/g.test(data)) {
-			var crc = data.replace(/.*([a-fA-F0-9]{2})\s:\scrc=([0-9]+).*\n.*/gmi, '$1-$2').split('-')			
-			if(crc[0] === crc[1]) {
-				return Promise.resolve(data)
-			} else {
-				throw new Error('crc failed : bytes don\'t correspond')
-			}
-		} else {
-			throw new Error('crc failed : Crc = NO')
-		}
-	}
-	
-	Thermometer.prototype.getTemperatureFromData = function(data) {
-		return this.checkCrc(data).then(function(data){
-			if(data.match(/.*t=([0-9])/g)) {
-				var temp = data.replace(/.*\s.*t=([0-9]+)/, '$1')/1000
-				temp = parseFloat(temp)
-				return Promise.resolve(temp)
-			} else {
-				throw new Error('data non conforme')
-			}
-		})
-	}
+    Thermometer.prototype.checkCrc = function(data) {        
+        if(/.*([a-fA-F0-9]{2})\s:\scrc=([0-9]+)\sYES/g.test(data)) {
+            var crc = data.replace(/.*([a-fA-F0-9]{2})\s:\scrc=([0-9]+).*\n.*/gmi, '$1-$2').split('-')            
+            if(crc[0] === crc[1]) {
+                return Promise.resolve(data)
+            } else {
+                throw new Error('crc failed : bytes don\'t correspond')
+            }
+        } else {
+            throw new Error('crc failed : Crc = NO')
+        }
+    }
+    
+    Thermometer.prototype.getTemperatureFromData = function(data) {
+        return this.checkCrc(data).then(function(data){
+            if(data.match(/.*t=([0-9])/g)) {
+                var temp = data.replace(/.*\s.*t=([0-9]+)/, '$1')/1000
+                temp = parseFloat(temp)
+                return Promise.resolve(temp)
+            } else {
+                throw new Error('data non conforme')
+            }
+        })
+    }
 
-	module.exports = Thermometer
+    module.exports = Thermometer
 }())
